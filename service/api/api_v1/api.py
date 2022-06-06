e710d13129fbe0dea1f254c17eb513fea858163d
tddmdde <technodom.dmd.de@gmail.com>
2022-06-06 14:54:10 +0600

    initial commit

--- service/api/api_v1/api.py
+++ service/api/api_v1/api.py
@@ -3,14 +3,14 @@ from fastapi import APIRouter
 
 # from .endpoints.urls import router as router_url
 # from .endpoints.smm import router as router_smm
-from .endpoints.home import router as router_home
+# from .endpoints.home import router as router_home
 # from .endpoints.seo import router as router_seo
 # from .endpoints.analysis import router as router_analysis
 # from .endpoints.direct import router as router_direct
 # from .endpoints.custom import router as router_custom
 # from .endpoints.content import router as router_content
 # from .endpoints.promotion import router as router_promotion
-# from .endpoints.visuals import router as router_visuals
+from .endpoints.visuals import router as router_visuals
 # from .endpoints.map_events import router as router_map_events
 # from .endpoints.map_events_edit_delete import app as router_map_events_edit_delete
 # from .endpoints.segmentator import router as router_segmentator
